7b76ddda1c01522ddc5899710b31962114bd6314
Mahmoudsaadlebda1997 <mahmodsaadlebda@gmail.com>
2024-03-13 03:55:58 +0200

    adding fixes

--- app/Http/Controllers/Admin/HotelController.php
+++ app/Http/Controllers/Admin/HotelController.php
@@ -11,7 +11,7 @@ class HotelController extends Controller
 {
     public function index()
     {
-        $hotels = Hotel::paginate(1);
+        $hotels = Hotel::paginate(5);
         $active = 'hotels';
 
         return view('admin.hotels.index', compact('hotels', 'active'));
