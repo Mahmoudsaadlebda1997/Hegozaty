c3426068ec45eea0ecf36d390c885b6ec2e19b99
Mahmoudsaadlebda1997 <mahmodsaadlebda@gmail.com>
2025-03-15 14:52:31 +0200

    adding last

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Hegozaty App - Banking Services</title>
+    <title>حجوزاتي للخدمات البنكيه</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
